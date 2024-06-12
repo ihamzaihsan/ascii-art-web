0e32a7af4b4088e4eda0881979fe44755619ecd2
ihamzaihsan <hamzaihsan.official@gmail.com>
2024-06-12 21:26:52 +0300

    commit

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "Templates/index.html")
+	})
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
